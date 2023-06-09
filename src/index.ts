c633d214b1c4d1bf2688b55f7a4b0a257f025c70
Gwion Tangi <lil.smiss7@gmail.com>
2023-06-10 00:08:00 +0200

    vote for your firstname (one time only now)

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,21 @@ const nameInput = new UInput();
 const name = nameInput.prompt('What is your name? ');
 
 const targets = [
-  'title',
-  'h1'
+  '.meaning__notfound',
+  '.section__greybold',
 ];
 const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded'
 };
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("vote", "10");
 
 const body = {
+  urlencoded
 };
 
-const response = UFetch('/prenom/' + name, 'GET', headers, body);
+const response = UFetch('/vote/' + name, 'POST', headers, body);
 
 response.then((data) => {
   const results = UParse(data, ...targets);
