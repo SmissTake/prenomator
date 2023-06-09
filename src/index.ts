320091f4917b692469207b993e4f024ae0d75c0f
Gwion Tangi <lil.smiss7@gmail.com>
2023-06-09 23:27:38 +0200

    added parser to return html title and h1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 require('dotenv').config({ path: '.env.local' });
 import UFetch from "./utils/UFetch";
+import UParse from "./utils/UParse";
 
-UFetch('/prenom/Gwion');
+const response = UFetch('/prenom/Gwion');
+
+const targets = [
+  'title',
+  'h1'
+];
+
+response.then((data) => {
+  UParse(data, ...targets);
+});
